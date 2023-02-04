27ac9e4ffd1336cfad893a0e2ab73a483ec98122
Rosario Henry <104395752+HenryRosario@users.noreply.github.com>
2023-02-04 19:55:46 -0400

    Add files via upload

---
+++ Registro_Personas/Migrations/20230203034120_inicial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroPersonas.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    PersonaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NombreCompleto = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<int>(type: "INTEGER", nullable: false),
+                    Celular = table.Column<int>(type: "INTEGER", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", nullable: false),
+                    FechaNacimiento = table.Column<DateTimeOffset>(type: "TEXT", nullable: false),
+                    OcupacionId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.PersonaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
---
+++ Registro_Personas/Models/Personas.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")]
+        public string? NombreCompleto { get; set; }
+
+        [Required(ErrorMessage ="El Telefono es requerido")]
+        public int Telefono { get; set; }
+
+        [Required(ErrorMessage ="El Celular es requerido")]
+        public int Celular { get; set; }
+
+        [Required(ErrorMessage ="El Email es requerido")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage ="La Direccion es requerido")]
+        public string? Direccion { get; set; }
+
+        [Required(ErrorMessage ="La Fecha es requerido")]
+        public DateTimeOffset FechaNacimiento { get; set; }
+
+        [ForeignKey("OcupacionId")]
+        public int OcupacionId { get; set; }
+}
